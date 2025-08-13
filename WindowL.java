653abf10558ae8191f97e9be6c06970d0251b266
Isa-36 <isabelajaviel@gmail.com>
2025-08-13 04:43:27 -0300

    Add files via upload

---
+++ WindowL.java
@@ -0,0 +1,60 @@
+package withListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+public class WindowL extends JFrame{
+	
+	public WindowL() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(5, 2, 10, 10));
+		
+		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 this.setTitle("DKNCKEBHJJ");
+		 this.add(panel, BorderLayout.CENTER);
+		 this.setSize(800, 600);
+		 this.setLocationRelativeTo(null);
+		    
+			JTextField JTF = new JTextField();       
+			JTextField JTF2 = new JTextField();
+			JTextField CI = new JTextField();
+			
+			JSpinner spinner = new JSpinner(new SpinnerNumberModel(18, 0, 120, 1));
+			
+			JButton boton = new JButton ("Mostrar");
+		 boton.setBounds(80,70,140,30);
+		 
+		    panel.add(new JLabel("Nombre "));
+			panel.add(JTF);
+			panel.add(new JLabel("Apellido"));
+			panel.add(JTF2);
+			panel.add(new JLabel("CI"));
+			panel.add(CI);
+			panel.add(new JButton("Aceptar"), BorderLayout.SOUTH);
+			panel.add(boton, BorderLayout.SOUTH);	
+		 
+		//Creamos un oyente para el boton
+		boton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String n = JTF.getText();
+				String a = JTF2.getText(); 
+				String ci = CI.getText();
+				int edad = (int)spinner.getValue();
+				
+				  System.out.println("Nombre: " + n);
+	                System.out.println("Apellido: " + a);
+	                System.out.println("CI: " + ci);
+	                System.out.println("Edad: " + edad);
+
+				
+				
+				} });  
+		this.setVisible(true);
+
+}
+    public static void main(String[] args) {
+        new WindowL();
+
+	}}
---
+++ mainL.java
@@ -0,0 +1,13 @@
+package withListener;
+import javax.swing.*;
+
+import withListener.WindowL;
+public class mainL extends WindowL {
+
+	   public static void mainL(String[] args) {
+	        WindowL WL = new WindowL();
+	        WL.setVisible(true);
+	       
+	}
+}
+
